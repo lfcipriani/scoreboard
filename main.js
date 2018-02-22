bf1e3a03b0852eb48625e31ba665b89f47bcaf59
Luis Cipriani <lfcipriani@gmail.com>
2018-02-22 23:43:20 +0100

    Lintint initial code

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const electron = require('electron')
+const electron = require("electron")
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-const path = require('path')
-const url = require('url')
+const path = require("path")
+const url = require("url")
 
 let mainWindow
 
@@ -11,15 +11,15 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'app/index.html'),
-    protocol: 'file:',
+    pathname: path.join(__dirname, "app/index.html"),
+    protocol: "file:",
     slashes: true
   }))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -30,17 +30,17 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
-// app.on('window-all-closed', function () {
+// app.on("window-all-closed", function () {
 //   // On OS X it is common for applications and their menu bar
 //   // to stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') {
+//   if (process.platform !== "darwin") {
 //     app.quit()
 //   }
 // })
 //
-// app.on('activate', function () {
+// app.on("activate", function () {
 //   // On OS X it's common to re-create a window in the app when the
 //   // dock icon is clicked and there are no other windows open.
 //   if (mainWindow === null) {
