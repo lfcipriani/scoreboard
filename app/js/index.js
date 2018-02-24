5f5beb2525561a7f036bc16231aeb22d71648d6f
Luis Cipriani <lfcipriani@gmail.com>
2018-02-24 17:28:55 +0100

    Building simple score keeping

---
+++ app/js/index.js
@@ -0,0 +1,14 @@
+const Score = require("../lib/score")
+
+$(function() {
+  let teams = ["a", "b"]
+
+  for (let t of teams) {
+    let teamScore = new Score($(`#sb-team-${t}-score-total`))
+    for (let i = 1; i <= 3; i++) {
+      $(`#sb-team-${t}-action-${i}`).click(
+        () => teamScore.doIt(i)
+      )
+    }
+  }
+});
