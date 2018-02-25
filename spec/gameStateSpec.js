a0f4deedeb2075596602867bce3550021f79f066
Luis Cipriani <lfcipriani@gmail.com>
2018-02-25 13:11:53 +0100

    Adding more info to GameState (countingDownTo field)

--- spec/gameStateSpec.js
+++ spec/gameStateSpec.js
@@ -6,7 +6,8 @@ describe("GameState", () => {
     let schedule = { start: null, end: null }
     let state = new GameState(schedule)
 
-    expect(state.current()).toBe("not_configured")
+    expect(state.current().state).toBe("not_configured")
+    expect(state.current().countingDownTo).toBe(null)
   })
 
   it("should return 'planned' if current time < start time", () => {
@@ -14,10 +15,12 @@ describe("GameState", () => {
     let now = new Date("2018/02/25 00:30:00").getTime()
     let state = new GameState(schedule)
 
-    expect(state.current(now)).toBe("planned")
+    expect(state.current(now).state).toBe("planned")
+    expect(state.current(now).countingDownTo).toBe("2018/02/25 01:00:00")
 
     now = new Date("2018/02/25 00:59:59").getTime()
-    expect(state.current(now)).toBe("planned")
+    expect(state.current(now).state).toBe("planned")
+    expect(state.current(now).countingDownTo).toBe("2018/02/25 01:00:00")
   })
 
   it("should return 'ongoing' if start time <= current time < end time", () => {
@@ -25,10 +28,12 @@ describe("GameState", () => {
     let now = new Date("2018/02/25 01:00:00").getTime()
     let state = new GameState(schedule)
 
-    expect(state.current(now)).toBe("ongoing")
+    expect(state.current(now).state).toBe("ongoing")
+    expect(state.current(now).countingDownTo).toBe("2018/02/25 02:00:00")
 
     now = new Date("2018/02/25 01:59:59").getTime()
-    expect(state.current(now)).toBe("ongoing")
+    expect(state.current(now).state).toBe("ongoing")
+    expect(state.current(now).countingDownTo).toBe("2018/02/25 02:00:00")
   })
 
   it("should return 'finished' if current time > end time", () => {
@@ -36,7 +41,10 @@ describe("GameState", () => {
     let now = new Date("2018/02/25 02:00:00").getTime()
     let state = new GameState(schedule)
 
-    expect(state.current(now)).toBe("finished")
-    expect(state.current()).toBe("finished")
+    expect(state.current(now).state).toBe("finished")
+    expect(state.current(now).countingDownTo).toBe(null)
+
+    expect(state.current().state).toBe("finished")
+    expect(state.current().countingDownTo).toBe(null)
   })
 })
